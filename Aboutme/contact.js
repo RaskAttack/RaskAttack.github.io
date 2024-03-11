eb367b03f5c14e338c88216357c118fe297309f7
RaskAttack <151085155+RaskAttack@users.noreply.github.com>
2024-03-11 14:07:27 -0600

    Update contact.js

--- Aboutme/contact.js
+++ Aboutme/contact.js
@@ -9,13 +9,13 @@ function sendEmail() {
   const bodyMessage = `Full Name : ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${mess.value}`;
   
   Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : "jacob@raskguitars.com",
-    Password : "FCA823CDDCC9097326B9E97ADBD4ACA0D571",
-    To : 'jacob@raskguitars.com',
-    From : "you@isp.com",
-    Subject : subject.value,
-    Body : bodyMessage
+    Host: "smtp.elasticemail.com",
+    Username: "jacob@raskguitars.com",
+    Password: "FCA823CDDCC9097326B9E97ADBD4ACA0D571",
+    To: 'jacob@raskguitars.com',
+    From: "you@isp.com",
+    Subject: subject.value,
+    Body: bodyMessage
 }).then(
   message => {
     if (message == "OK") {
